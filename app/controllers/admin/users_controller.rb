103cf65d5e06c6d677c41b0014c2c0456e6474f5
GrahamSM <graham.sm94@gmail.com>
2016-05-24 22:19:07 +0000

    User management enhancement

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,17 @@
 class Admin::UsersController < ActionController::Base
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
+  def show
+    binding.pry
+    @user = User.find(params[:id])
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation)
+  end
 end
