03bc3e4fb1f4dd10d1de3d3c27f4f01abf907a3f
GrahamSM <graham.sm94@gmail.com>
2016-05-24 16:43:03 +0000

    generating reviews controller and reviews/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   resources :movies do
-
     resources :reviews, only: [:new, :create]
-
   end
   resources :users, only: [:new, :create]
 
