c97df3b02334ad7285ed6e9e3e42b82d7956e572
GrahamSM <graham.sm94@gmail.com>
2016-05-26 13:56:34 +0000

    Updated admin functionality to display message and restrict access

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,11 +1,14 @@
 class Admin::ApplicationController < ActionController::Base
 
+  before_action :check_admin
   helper_method :current_user, :can_switch_view?
 
-  private
+  protected
+
   def check_admin
     unless current_user.admin?
-      redirect_to index
+      flash[:notice] = "You must be an admin to view that page"
+      redirect_to root_path
     end
   end
 
@@ -16,5 +19,11 @@ class Admin::ApplicationController < ActionController::Base
       false
     end
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+
 
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ActionController::Base
+class Admin::UsersController < Admin::ApplicationController
 
   def index
     @users = User.all
